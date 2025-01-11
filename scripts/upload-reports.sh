0043cfb08969ca50ea6aebce01895cd3f97dab5e
Diego Parra <diegomiguelp1996@gmail.com>
2025-01-12 00:47:55 +0100

    Add recursive flag to gcp command in upload-reports

--- scripts/upload-reports.sh
+++ scripts/upload-reports.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-echo "Upload reports"
 HASH=$(git rev-parse --short HEAD)
-echo "This is the hash $HASH"
 gcloud config set project "$GCLOUD_PROJECT"
-gcloud storage cp ./html gs://"$REPORTER_BUCKET/reports/$HASH"
-ls
-echo "This is the hash $GCLOUD_PROJECT"
+gcloud storage cp --recursive ./html gs://"$REPORTER_BUCKET/reports/$HASH"
 echo "$REPORTER_URL/$HASH"

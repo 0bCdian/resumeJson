85ee84f2ccb11fea3583e3797849a94aa91857b3
Diego Parra <diegomiguelp1996@gmail.com>
2025-01-12 00:43:18 +0100

    add debug echos to upload-report.sh

--- scripts/upload-reports.sh
+++ scripts/upload-reports.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-
+echo "Upload reports"
 HASH=$(git rev-parse --short HEAD)
+echo "This is the hash $HASH"
 gcloud config set project "$GCLOUD_PROJECT"
 gcloud storage cp ./html gs://"$REPORTER_BUCKET/reports/$HASH"
+ls
+echo "This is the hash $GCLOUD_PROJECT"
 echo "$REPORTER_URL/$HASH"

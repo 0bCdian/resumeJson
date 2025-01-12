95b739d373db63f0993bdb8f9137c6c16f2c8fa8
Diego Parra <diegomiguelp1996@gmail.com>
2025-01-12 21:14:20 +0100

    refactor: gh actions and scripts

--- scripts/detect-changes.sh
+++ scripts/detect-changes.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 DIR=$1
+CURRENT_REF=$2
+PREVIOUS_REF=$3
 
 if [[ -z "$DIR" ]]; then
   echo "Empty dir"
@@ -10,11 +12,13 @@ fi
 
 if [[ "$DIR" == "frontend" ]]; then
   DIR="packages/frontend packages/frontend_server"
+if [[ "$DIR" == "test-reporter" ]];then
+  DIR="./Dockerfile.reporter ./nginx.conf"
 else
   DIR="packages/$DIR"
 fi
 
-if git diff --quiet HEAD HEAD~1 -- "$DIR"; then
+if git diff --quiet "$CURRENT_REF" "$PREVIOUS_REF"  -- "$DIR"; then
   echo "CHANGED=false" >>"$GITHUB_ENV"
 else
   echo "CHANGED=true" >>"$GITHUB_ENV"
